a1f4c2c942489773eef0344a9c3ce672d7417b04
James7c8 <125234843+James7c8@users.noreply.github.com>
2023-03-05 20:10:22 -0500

    Agregar dos productos

--- src/main/java/l3/tienda/model/bootstrap/Inicializador.java
+++ src/main/java/l3/tienda/model/bootstrap/Inicializador.java
@@ -2,6 +2,7 @@ package l3.tienda.model.bootstrap;
 
 import l3.tienda.model.entity.Producto;
 import l3.tienda.model.DAO.ProductoDAO;
+import l3.tienda.model.service.ProductoService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -9,16 +10,20 @@ import org.springframework.stereotype.Component;
 // CommandLineRunner es una interfaz que proporciona el único método `run(String... args)`.
 public class Inicializador implements CommandLineRunner {
 
-    private final ProductoDAO productoDAO;
+    private final ProductoService productoService;
 
-    public Inicializador(ProductoDAO productoDAO) {
-        this.productoDAO = productoDAO;
+    public Inicializador(ProductoService productoService) {
+        this.productoService = productoService;
     }
 
 
     @Override
     public void run(String... args) throws Exception {
+        // Se crean unos productos para tener algo en la BD.
         Producto producto1 = new Producto("manzana", 1000, 20, true, "frutas y verduras");
+        productoService.agregarProducto(producto1);
 
+        // En comparación con producto1 este se implementa en una sola linea para ahorrar espacio.
+        productoService.agregarProducto(new Producto("banano", 500, 70, true, "frutas y verduras"));
     }
 }
--- src/main/java/l3/tienda/model/service/ProductoService.java
+++ src/main/java/l3/tienda/model/service/ProductoService.java
@@ -1,13 +1,12 @@
 package l3.tienda.model.service;
 
-import jdk.incubator.vector.VectorOperators;
+import java.util.List;
+
 import l3.tienda.model.DAO.ProductoDAO;
 import l3.tienda.model.entity.Producto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 // Un service proporciona una funcionalidad o capacidad específica a otras partes de un programa. Los servicios están
 // diseñados a ser 'loosely coupled' para que puedan sustituirse o modificarse fácilmente sin afectar a otras partes del
@@ -22,29 +21,29 @@ public class ProductoService {
 
     // @Transactional gestiona automáticamente las transacciones de la base de datos. Se aplica en métodos o clases que
     // implican operaciones de base de datos, como leer de, escribir en o borrar datos de una base de datos.
-    // 
+    //
     // Cuando se ejecuta un método con @Transactional, se inicia automáticamente una transacción antes de que el método
     // se ejecute y se confirma después de que el método se complete con éxito. Si el método falla, la transacción es
     // rolled back, lo que significa que cualquier cambio realizado en la base de datos es revertido.
     // Esto ayuda a garantizar que la base de datos se mantiene consistente incluso si varios usuarios acceden a ella
     // simultáneamente.
     @Transactional(readOnly = true)  // las consultas llevan readOnly = true
-    public List<Producto> buscarAsignaturasTodas() {
+    public List<Producto> buscarProductosTodos() {
         return productoDAO.findAll();
     }
 
     @Transactional(readOnly = true)
-    public Producto buscarAsignaturaPorId(Integer id) {
+    public Producto buscarProductoPorId(Integer id) {
         return productoDAO.findById(id).orElse(null);
     }
 
     @Transactional
-    public void agregarAsignatura(Producto producto) {
+    public void agregarProducto(Producto producto) {
         productoDAO.save(producto);
     }
 
     @Transactional
-    public void eliminarAsignatura(Integer id) {
+    public void eliminarProducto(Integer id) {
         productoDAO.deleteById(id);
     }
 
